65eacfeed963483cfafdcc34a6a71e9432efe3e8
iBroadband <agimmi00@gmail.com>
2022-04-01 03:44:25 -0700

    Add lib

---
+++ lib/Settings_Module.py
@@ -0,0 +1,32 @@
+import os
+import codecs
+import json
+
+class MySettings(object):
+	def __init__(self, settingsfile=None):
+		try:
+			with codecs.open(settingsfile, encoding="utf-8-sig", mode="r") as f:
+				self.__dict__ = json.load(f, encoding="utf-8")
+		except:
+			#   Default Settings
+			self.Command = "!replay"
+			self.Hotkey = "F12"
+			self.InstantReplayScene = "Destiny 2 (Replay Display)"
+			self.ReplayDuration = 30
+			self.Permission = "everyone"
+			self.Threshold = 3
+			self.UserCooldown = 30
+
+	def Reload(self, jsondata):
+		self.__dict__ = json.loads(jsondata, encoding="utf-8")
+		return
+
+	def Save(self, settingsfile):
+		try:
+			with codecs.open(settingsfile, encoding="utf-8-sig", mode="w+") as f:
+				json.dump(self.__dict__, f, encoding="utf-8")
+			with codecs.open(settingsfile.replace("json", "js"), encoding="utf-8-sig", mode="w+") as f:
+				f.write("var settings = {0};".format(json.dumps(self.__dict__, encoding='utf-8')))
+		except:
+			Parent.Log(ScriptName, "Failed to save settings to file.")
+		return
